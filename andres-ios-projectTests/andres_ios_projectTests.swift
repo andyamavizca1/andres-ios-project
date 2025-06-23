d03127a839c9a1e0489e7879bfcf2e96109e9cbe
Andres Amavizca <andres.amavizca@cat.com>
2025-06-23 11:42:13 -0700

    Initial Commit

---
+++ andres-ios-projectTests/andres_ios_projectTests.swift
@@ -0,0 +1,17 @@
+//
+//  andres_ios_projectTests.swift
+//  andres-ios-projectTests
+//
+//  Created by andres.amavizca on 23/06/25.
+//
+
+import Testing
+@testable import andres_ios_project
+
+struct andres_ios_projectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
