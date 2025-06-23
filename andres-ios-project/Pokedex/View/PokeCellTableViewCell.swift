d7271d572eed225d8a0408f4b1403d5080413f51
andresamavizca1 <andres.amavizca@accenture.com>
2025-06-23 13:16:10 -0700

    Crear y hacer setup de las views

---
+++ andres-ios-project/Pokedex/View/PokeCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PokeCellTableViewCell.swift
+//  andres-ios-project
+//
+//  Created by andres.amavizca on 23/06/25.
+//
+
+import UIKit
+
+class PokeCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
